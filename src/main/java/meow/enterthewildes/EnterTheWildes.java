6eb4753e5844624612edc7ea6f88c1078aaef554
cowie <cowiedan2007@icloud.com>
2025-11-09 20:05:39 +0000

    ˚

--- src/main/java/meow/enterthewildes/EnterTheWildes.java
+++ src/main/java/meow/enterthewildes/EnterTheWildes.java
@@ -1,14 +1,14 @@
 package meow.enterthewildes;
 
-import meow.enterthewildes.item.ModItems;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.world.item.Item;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.core.Registry;
 
 public class EnterTheWildes implements ModInitializer {
-    public static final String MOD_ID = "enterthewildes";
-
     @Override
     public void onInitialize() {
-        ModItems.register();
-        ModItemGroups.register();
+        System.out.println("initialised");;
     }
 }
